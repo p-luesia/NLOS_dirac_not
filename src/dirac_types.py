8a048e379eda3e4b8267c9aca22823b48afafbea
Pablo Luesia <pluesia@unizar.es>
2023-02-16 11:22:08 +0100

    Convolutions to complete output

--- src/dirac_types.py
+++ src/dirac_types.py
@@ -14,17 +14,21 @@ def dagger(vector_states):
     return np.conjugate(vector_states)
 
 
-def fftn_but_first(data):
+def fftn_but_first(data, o_shape):
     """
     It return the Fourier transform along all the axes of the data but the
     first one, and it returns twice -1 the given shape
     @param data             : Data to transform
+    @param o_shape          : Output shape of the data for axes from 1 to n.
+                              If larger than the input, it pads with zeros, if 
+                              lower, it trim the input
     @return (f_data, axes)  : f_data is the data in the fourier domain but the
                               first axes, and axes are the axes to transform
     """      
-    s = np.array(data.shape)[1:]
+    assert o_shape.shape[0] == data.ndim-1,\
+        "The output shape does not mach with the dimensions of the data"
     axes = np.arange(1, data.ndim)
-    f_data = np.fft.fftn(data, s = s, axes = axes)
+    f_data = np.fft.fftn(data, s = o_shape, axes = axes)
     return (f_data, axes)
 
 
@@ -77,15 +81,16 @@ class Ket(np.ndarray):
                 "Bra and Ket shapes do not match"
 
         # Data to Fourier domain
-        f_ket, axes = fftn_but_first(self)
-        f_bra, _ = fftn_but_first(bra)
+        o_shape = shape_bra[1:]*2 - 1
+        f_ket, axes = fftn_but_first(self, o_shape)
+        f_bra, _ = fftn_but_first(bra, o_shape)
 
         # Outter product with convolutions in Fourier
         f_o = f_ket[:, np.newaxis, ...] * f_bra[np.newaxis, ...]
 
         # The output expected shape
         o_shape = tuple(shape_bra[1:])
-        return np.fft.ifftn(f_o, s=o_shape, axes = axes+1).view(np.ndarray).copy()
+        return np.fft.ifftn(f_o, s=o_shape, axes = axes+1).view(np.ndarray)#.copy()
 
     def __str__(self):
         """
@@ -103,11 +108,11 @@ class Ket(np.ndarray):
         """
         return self.outer(bra)
 
-    def __rmul__(self, term):
-        """
-        Multiply operator override
-        """
-        return np.matmul(term, self)
+    # def __rmul__(self, term):
+    #     """
+    #     Multiply operator override
+    #     """
+    #     return np.matmul(term, self)
 
 
 class Bra(np.ndarray):
